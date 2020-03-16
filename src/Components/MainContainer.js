047408d5a9a3bfcc522c0b00c443297a2272846a
Vipul <vipul@passage.ai>
2020-03-15 22:25:53 -0700

    Fix css bug and update packages

--- src/Components/MainContainer.js
+++ src/Components/MainContainer.js
@@ -97,7 +97,7 @@ class MainContainer extends Component {
                             <img className="github" src="https://camo.githubusercontent.com/38ef81f8aca64bb9a64448d0d70f1308ef5341ab/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f6461726b626c75655f3132313632312e706e67" alt="Fork me on GitHub" /></a>)
                         :(<span></span>)}
 
-                    <div className="content overflow-hidden">
+                    <div className={this.state.pageValue == 'home' ? "content  overflow-hidden" : "content" }>
                         <span className="code">&lt;html&gt;</span><br/>
                         <span className="code">&lt;body&gt;</span><br/>
                             {this.state.page}
